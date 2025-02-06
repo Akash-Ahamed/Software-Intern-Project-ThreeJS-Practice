8d3bcf0475f5d388676cc531f8e1834a63e18054
Akash-Ahamed <akash.ahamed.engr@gmail.com>
2025-02-06 16:01:02 +0600

    Added next.js home page code

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import FirstScene from "./firstScene/page";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <FirstScene />
+      </main>
+    </div>
+  );
+}
